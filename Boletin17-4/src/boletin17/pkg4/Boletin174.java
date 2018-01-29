88ffd09ef05199a55babc04e7bf3128a0ea8bb58
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-01-29 08:35:00 +0100

    COMMIT GITHUB

---
+++ Boletin17-4/src/boletin17/pkg4/Boletin174.java
@@ -0,0 +1,21 @@
+
+package boletin17.pkg4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author David
+ */
+public class Boletin174 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos_4 obx=new Metodos_4();
+        //Visualiza a letra do número do DNI que se pide por teclado e o DNI completo:
+        obx.devolverLetra();
+    }
+    
+}
