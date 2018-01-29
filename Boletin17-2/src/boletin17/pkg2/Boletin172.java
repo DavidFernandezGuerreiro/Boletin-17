1353ef70781d556efe1a5cd568a817b79febcec7
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-01-29 08:34:13 +0100

    COMMIT GITHUB

---
+++ Boletin17-2/src/boletin17/pkg2/Boletin172.java
@@ -0,0 +1,24 @@
+
+package boletin17.pkg2;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin172 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos_2 obx=new Metodos_2();
+        //Visualiza los suspensos y los aprobados:
+        obx.visualizar();
+        //Visualiza la nota media de todos los alumnos:
+        obx.notaMedia();
+        //Visualiza la nota más alta:
+        obx.notaAlta();
+    }
+    
+    
+}
